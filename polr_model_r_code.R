6a3a667fc8164a4fbd7e1f6345132276fb23524b
cheriemosher <mosher.cherie@hushmail.com>
2020-02-24 22:08:47 -0800

    made a folder for model coefficient csv files.  Ran the polr model for ageing and spiders.

--- polr_model_r_code.R
+++ polr_model_r_code.R
@@ -1,12 +1,14 @@
 install.packages("MASS")
+
 library(MASS)
+
 phobia_personality_data = read.csv("Data/personality_traits.csv", header = TRUE)
 View(phobia_personality_data)
 
-model_fit <- polr(heights ~ Daily_events + Prioritising_workload + Writing_notes + Workaholism +
+model_fit <- polr(Spiders ~ Daily_events + Prioritising_workload + Writing_notes + Workaholism +
                     Thinking_ahead + Final_judgement + Reliability + Keeping_promises + Loss_of_interest + 
                     Friends_versus_money + Funniness + Fake + Criminal_damage + Decision_making + 
-                    Elections + Self.criticism + Judgment_calls + Hypochondria + Empathy + Eating_to_survive + 
+                    Elections + Self_criticism + Judgment_calls + Hypochondria + Empathy + Eating_to_survive + 
                     Giving + Compassion_to_animals + Borrowed_stuff + Loneliness + Cheating_in_school + 
                     Health + Changing_the_past + God + Dreams	+ Charity + Number_of_friends + Waiting +
                     New_environment + Mood_swings + Appearence_and_gestures + Socializing + Achievements + 
@@ -22,4 +24,4 @@ pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
 summary_table <- cbind(summary_table, "p value" = round(pval,3))
 summary_table
 
-write.csv(summary_table,"polr_model_coef_heights_MOSHER.csv", row.names = TRUE)
+write.csv(summary_table,"polr_model_coef_spiders_MOSHER.csv", row.names = TRUE)
