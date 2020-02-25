ab20f25a18c4e8faf8c3befb08f58bb81b978f99
cheriemosher <mosher.cherie@hushmail.com>
2020-02-24 22:52:10 -0800

    added r script for binomial logistic regression.  Added a folder for model coefficient outputs.  Ran binomial regression for gender and multinomial regression for education level

---
+++ glm_binomial_model_r_code.R
@@ -0,0 +1,23 @@
+library(MASS)
+
+data = read.csv("Data/personality_traits.csv", header = TRUE)
+View(data)
+
+model_fit <- glm(Gender ~ Daily_events + Prioritising_workload + Writing_notes + Workaholism +
+                   Thinking_ahead + Final_judgement + Reliability + Keeping_promises + Loss_of_interest + 
+                   Friends_versus_money + Funniness + Fake + Criminal_damage + Decision_making + 
+                   Elections + Self_criticism + Judgment_calls + Hypochondria + Empathy + Eating_to_survive + 
+                   Giving + Compassion_to_animals + Borrowed_stuff + Loneliness + Cheating_in_school + 
+                   Health + Changing_the_past + God + Dreams	+ Charity + Number_of_friends + Waiting +
+                   New_environment + Mood_swings + Appearence_and_gestures + Socializing + Achievements + 
+                   Responding_to_a_serious_letter + Children + Assertiveness + Getting_angry + 
+                   Knowing_the_right_people + Public_speaking + Unpopularity + Life_struggles + Happiness_in_life +
+                   Energy_levels + Small_big_dogs + Personality + Finding_lost_valuables + Getting_up + Interests_or_hobbies +
+                   Parents_advice + Questionnaires_or_polls, data = phobia_personality_data, family="binomial")
+
+summary_table <- coef(summary(model_fit))
+summary_table2 <- summary(model_fit)
+summary_table2
+
+
+write.csv(summary_table,"bi_logic_model_coef_gender_MOSHER.csv", row.names = TRUE)
